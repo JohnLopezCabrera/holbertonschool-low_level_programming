c1050465150581884e1980ba7f6176e89c28e502
JohnLopezCabrera <johnelectric90@outlook.com>
2024-02-24 13:14:24 -0400

    l

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - lala
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+int = n;
+for (n = 'a'; n <= 'z'; n++)
+{
+_putchar(n);
+}
+_putchar('\n');
+return (0);
+}
