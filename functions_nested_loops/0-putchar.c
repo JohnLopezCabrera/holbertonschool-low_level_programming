f12c3f71a3f32348412ede9322806717a69fc1bc
JohnLopezCabrera <johnelectric90@outlook.com>
2024-02-24 12:10:05 -0400

    l

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,16 +1,16 @@
-#include <main.h>
+#include "main.h"
 
 int main(void)
 {
-   _ putchar('_');
-   _ putchar('p');
-   _ putchar('u');
-   _ putchar('t');
-   _ putchar('c');
-   _ putchar('h');
-   _ putchar('a');
-   _ putchar('r');
-   _ putchar('\n');
+   _putchar('_');
+   _putchar('p');
+   _putchar('u');
+   _putchar('t');
+   _putchar('c');
+   _putchar('h');
+   _putchar('a');
+   _putchar('r');
+   _putchar('\n');
 
     return 0;
 }
