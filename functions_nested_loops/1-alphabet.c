3f2791b4cffc49702a6bf4e5cb64537a3c4ff060
JohnLopezCabrera <johnelectric90@outlook.com>
2024-02-24 13:20:02 -0400

    l

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "main.h"
+
 /**
- * print_alphabet - lala
- *
- * Return: 0
+ * print_alphabet - Entry point
+ * Return:0
  */
 void print_alphabet(void)
 {
-int = n;
-for (n = 'a'; n <= 'z'; n++)
+char c = 'a';
+while (c <= 'z')
 {
-_putchar(n);
+_putchar(c);
+c++;
 }
 _putchar('\n');
-return (0);
 }
