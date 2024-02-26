e3ce42d8d00bd57fafdb07c4b3fedadfa8ce08a4
JohnLopezCabrera <johnelectric90@outlook.com>
2024-02-26 13:44:31 -0400

    l

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_numbers - prints the numbers, from 0 to 9,
+ *followed by a new line
+ */
+void print_numbers(void)
+{
+int c;
+{
+for (c = 0, c < 10, c++)
+{
+_putchar(c + '0')
+}
+putchar('\n');
+return (0);
+}
