b3e081d584f5a147ea9a2f76a997dc49a6b69d05
JohnLopezCabrera <johnelectric90@outlook.com>
2024-02-28 12:24:46 -0400

    l

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_strlen - returns the length of the string
+ * @string: string to evaluate
+ *
+ *Return:length
+ */
+int _strlen(char *string)
+{
+int length = 0;
+while (string[length] != '\0')
+{
+length++
+}
+return (length);
+}
