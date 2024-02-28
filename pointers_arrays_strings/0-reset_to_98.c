e4a5fce8e2b58af4ba590a81739257e06828b8ca
JohnLopezCabrera <johnelectric90@outlook.com>
2024-02-28 11:08:51 -0400

    l

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the variable value
+ * the pointer points towards 98
+ * @n: pointer to the variable update
+ */
+void reset_to_98(int *n);
+{
+*n = 98;
+}
