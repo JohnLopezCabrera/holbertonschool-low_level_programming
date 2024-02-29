395f974a598ce139756c5d30b8d3b59bedae6725
JohnLopezCabrera <johnelectric90@outlook.com>
2024-02-29 12:33:44 -0400

    l

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -6,11 +6,12 @@
  **/
 void puts2(char *str)
 {
-int i = 0;
-while (str[i] != '\0')
-{
-_putchar(str[i]);
-i += 2;
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        _putchar(str[i]);
+        i += 2;
+    }
+    _putchar('\n');
 }
-_putchar('\n');
-}
+
