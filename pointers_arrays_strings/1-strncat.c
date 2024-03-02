3c56172ba1d87d857feb31a6b080ab9fd329ac52
JohnLopezCabrera <johnelectric90@outlook.com>
2024-03-02 16:06:05 -0400

    l

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -8,8 +8,8 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
-		while (dest[i] != '\0')
+	int i = 0, j = 0;
+	while (dest[i] != '\0')
 		i++;
 	while (src[j] != '\0' && j < n)
 	{
