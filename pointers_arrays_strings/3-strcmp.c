e43e552ac747e95c22b603b00cc367fd02b3a64f
JohnLopezCabrera <johnelectric90@outlook.com>
2024-03-02 19:39:03 -0400

    l

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -7,7 +7,7 @@
  * Return: less than 0 if s1 is less than s2, 0 if equal,
  * more than 0 if s1 is greater than s2
  */
-int _strcmp(char *s1, chat *s2)
+int _strcmp(char *s1, char *s2)
 {
 	while (*s1 == *s2)
 	{
