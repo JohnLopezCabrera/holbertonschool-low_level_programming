b64bb25e5ac2b437aa5e2169135269cea4c52894
JohnLopezCabrera <johnelectric90@outlook.com>
2024-03-02 19:35:36 -0400

    l

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+
+#include "main.h"
+/**
+ * _strcmp - comapare
+ * @s1: 1st string
+ * @s2: second string
+ *
+ * Return: less than 0 if s1 is less than s2, 0 if equal,
+ * more than 0 if s1 is greater than s2
+ */
+int _strcmp(char *s1, chat *s2)
+{
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			return (0);
+			}
+			s1++;
+			s2++;
+		}
+		return (*s1 - *s2);
+		}
