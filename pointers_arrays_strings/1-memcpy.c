9eb8bd409d27db5b131f11d91360a706c5a21a41
JohnLopezCabrera <johnelectric90@outlook.com>
2024-03-04 17:39:46 -0400

    l

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memcopy - function
+ * @dest: pointer to the memory area to be filled
+ * @src: the value to be set
+ * @n: number of bytes to be set to the value
+ * * Return: a pointer to the memory area dest
+ */
+char *_memcopy(char *dest, char *src, unsigned int n)
+	{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	}
+	dest[i] = src[i];
+	}
+	return (dest);
+}
