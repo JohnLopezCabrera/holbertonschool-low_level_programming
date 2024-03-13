17ac61b6b68f4e7462f8635e715f21127859a96c
JohnLopezCabrera <johnelectric90@outlook.com>
2024-03-13 14:54:36 -0400

    l

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * free_grid - frees memory for the grid
+ * @grid: grid
+ * @height: height
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || height == 0)
+	return;
+
+	for (i = 0; i < height; i++)
+	free(grid[i]);
+
+	free(grid);
+}
