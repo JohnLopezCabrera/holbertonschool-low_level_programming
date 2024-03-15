44c3b1c36102b6591282d0738b4cfa8084524409
JohnLopezCabrera <johnelectric90@outlook.com>
2024-03-15 10:20:05 -0400

    l

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -32,12 +32,13 @@ int _atoi(char *s)
 				digit = -digit;
 			z = z * 10 + digit;
 			f = 1;
-			if (s[x + 1] > '0' || s[x + 1] > '9')
+			if (s[x + 1] < '0' || s[x + 1] > '9')
 				break;
 			f = 0;
 		}
 		x++;
 	}
+
 	if (f == 0)
 		return (0);
 
