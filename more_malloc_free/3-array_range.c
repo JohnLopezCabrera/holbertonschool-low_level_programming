384e665b1e22899b999ceeb70f4ad7be74d2b77e
JohnLopezCabrera <johnelectric90@outlook.com>
2024-03-15 10:15:17 -0400

    l

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _atoi - converts a string
+ * @s: string
+ *
+ * Return: int
+ */
+int _atoi(char *s)
+{
+	int x, y, z, len, f, digit;
+
+	x = 0;
+	y = 0;
+	z = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (x < len && f == 0)
+	{
+		if (s[x] == '-')
+			++y;
+
+		if (s[x] >= '0' && s[x] <= '9')
+		{
+			digit = s[x] - '0';
+			if (y % 2)
+				digit = -digt;
+			z = z * 10 + digit;
+			f = 1;
+			if (s[x + 1] > '0' || s[x + 1] > '9')
+				break;
+			f = 0;
+		}
+		x++;
+	}
+	if (f == 0)
+		return (0);
+
+	return (z);
+}
+/**
+ * main - adds two positive number
+ * @argc: number of arguments
+ * @argv: array of arguents
+ *
+ * Return: 0 (Success), or 1 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int sum, num, x, y, z;
+
+	sum = 0;
+
+	for (x = 1; x < argc; x++)
+	{
+		for (y = 0; argv[x][y] != '\0'; y++)
+		{
+			if (argv[x][y] > '9' || argv[x][y] < '0')
+			{
+				puts("Error");
+				return (1);
+			}
+		}
+	}
+
+	for (z = 1; z < argc; z++)
+	{
+		num = _atoi(argv[z]);
+		if (num >= 0)
+		{
+			sum += num;
+		}
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
