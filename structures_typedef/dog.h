62ae166c0a46be0073664c168f5a7af6c05a5605
JohnLopezCabrera <johnelectric90@outlook.com>
2024-03-16 09:48:40 -0400

    l

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -5,6 +5,7 @@
  * @name: First
  * @age: Second
  * @owner: Third
+ * Description: more details
  */
 struct dog
 {
@@ -13,6 +14,12 @@ float age;
 char *owner;
 };
 
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif /* DOG_H */
