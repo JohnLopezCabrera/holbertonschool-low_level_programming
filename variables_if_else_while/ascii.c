8dfdc039028a6a6ff9c989b2f3950b7500d971f1
JohnLopezCabrera <johnelectric90@outlook.com>
2024-03-22 06:18:21 -0400

    l

---
+++ variables_if_else_while/ascii.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=0;i<=150;i++)  // for loop from 65 to 90
+    {
+        printf("The ASCII value of %c character is %d\n", i,i);
+    }
+    return 0;
+}
