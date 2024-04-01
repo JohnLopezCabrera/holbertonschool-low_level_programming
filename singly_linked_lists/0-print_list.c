816862cf985d6f9e6d9a09c28db4be698f07ab42
JohnLopezCabrera <9043@Holbertonstudents.com>
2024-04-01 12:29:21 -0400

    l

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include "lists.h"
-
-size_t print_list(const list_t *h) {
+size_t print_list(const list_t *h)
+{
 	const list_t *current = h;
 	size_t count = 0;
 
-	while (current != NULL) {
-        if (current->str != NULL) {
-	printf("[%lu] %s\n", current->len, current->str);
-        } else {
+	while (current != NULL)
+	{
+		if (current->str != NULL)
+	{
+		printf("[%lu] %s\n", current->len, current->str);
+	}
+	else
+	{
 	printf("[0] (nil)\n");
 	}
 	current = current->next;
 	count++;
 	}
-	
-	return count;
+
+	return (count);
 }
