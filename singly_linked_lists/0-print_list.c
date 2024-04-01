e726b638f8f79a6c614280143155c50d4935219a
JohnLopezCabrera <9043@Holbertonstudents.com>
2024-04-01 12:23:01 -0400

    l

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+
+size_t print_list(const list_t *h) {
+	const list_t *current = h;
+	size_t count = 0;
+
+	while (current != NULL) {
+        if (current->str != NULL) {
+	printf("[%lu] %s\n", current->len, current->str);
+        } else {
+	printf("[0] (nil)\n");
+	}
+	current = current->next;
+	count++;
+	}
+	
+	return count;
+}
